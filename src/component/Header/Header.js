097024320b7d723e3e9dfbcc89eaacad2561f331
zunead <zuned1996@outlook.com>
2021-04-18 19:26:07 +0600

    first commit

---
+++ src/component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import './Header.css';
+const Header = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Header;
